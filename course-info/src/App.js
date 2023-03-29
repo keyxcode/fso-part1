57512b0bcf611048f725cf0c1a7eb7d4303524ae
keyxcode <keyxcode@gmail.com>
2023-03-29 07:02:09 +0700

    Solution 1.2: Refactor Content to smaller Part components

--- course-info/src/App.js
+++ course-info/src/App.js
@@ -2,11 +2,21 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part} {props.exercises}
     </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   );
 };
 
@@ -26,9 +36,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total num={exercises1 + exercises2 + exercises3} />
     </div>
   );
