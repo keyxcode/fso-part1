2154ddc881a938b7f0b485638b3b24df447779a7
keyxcode <keyxcode@gmail.com>
2023-03-30 17:11:26 +0700

    Add dummy buttons for unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,12 +1,23 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => console.log("good")} text="good" />
+      <Button handleClick={() => console.log("neutral")} text="neutral" />
+      <Button handleClick={() => console.log("bad")} text="bad" />
+    </div>
+  );
 };
 
 export default App;
