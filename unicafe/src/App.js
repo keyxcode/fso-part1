af8e2147fa6ea0d671caf3a818e18dce02ece5a4
keyxcode <keyxcode@gmail.com>
2023-03-30 17:39:40 +0700

    Solution 1.8 and 1.10: Refactor Stats component into Statistics and StatisticLine

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -4,11 +4,30 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Stats = ({ statName, statNum }) => (
+const StatisticsLine = ({ statName, statNum }) => (
   <div>
     {statName} {statNum}
   </div>
 );
+
+const Statistics = ({ good, neutral, bad }) => {
+  const totalRatings = good + neutral + bad;
+  const avgRating = (good * 1 + neutral * 0 + bad * -1) / totalRatings;
+  const positivePercentage = `${(good / totalRatings) * 100} %`;
+
+  if (totalRatings === 0) return <div>No feedback given</div>;
+
+  return (
+    <div>
+      <StatisticsLine statName="good" statNum={good} />
+      <StatisticsLine statName="neutral" statNum={neutral} />
+      <StatisticsLine statName="bad" statNum={bad} />
+      <StatisticsLine statName="all" statNum={totalRatings} />
+      <StatisticsLine statName="average" statNum={avgRating} />
+      <StatisticsLine statName="positive" statNum={positivePercentage} />
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -20,38 +39,16 @@ const App = () => {
   const increaseNeutralRating = () => setNeutral(neutral + 1);
   const increaseBadRating = () => setBad(bad + 1);
 
-  const totalRatings = good + neutral + bad;
-  const averageRating = (good * 1 + neutral * 0 + bad * -1) / totalRatings;
-  const positivePercentage = (good / totalRatings) * 100;
-
-  if (totalRatings !== 0) {
-    return (
-      <div>
-        <h1>Give feedback</h1>
-        <Button handleClick={increaseGoodRating} text="good" />
-        <Button handleClick={increaseNeutralRating} text="neutral" />
-        <Button handleClick={increaseBadRating} text="bad" />
-        <h1>Statistics</h1>
-        <Stats statName="good" statNum={good} />
-        <Stats statName="neutral" statNum={neutral} />
-        <Stats statName="bad" statNum={bad} />
-        <Stats statName="all" statNum={totalRatings} />
-        <Stats statName="average" statNum={averageRating} />
-        <Stats statName="positive" statNum={positivePercentage} />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Give feedback</h1>
-        <Button handleClick={increaseGoodRating} text="good" />
-        <Button handleClick={increaseNeutralRating} text="neutral" />
-        <Button handleClick={increaseBadRating} text="bad" />
-        <h1>Statistics</h1>
-        No feedback given
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={increaseGoodRating} text="good" />
+      <Button handleClick={increaseNeutralRating} text="neutral" />
+      <Button handleClick={increaseBadRating} text="bad" />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
 };
 
 export default App;
