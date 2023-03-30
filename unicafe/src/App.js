111cb136a394d21c087a6fc48fd6b1f61c1c02db
keyxcode <keyxcode@gmail.com>
2023-03-30 17:14:11 +0700

    Add dummy stats components to unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -4,6 +4,12 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const Stats = ({ statName, statNum }) => (
+  <div>
+    {statName} {statNum}
+  </div>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -12,10 +18,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <h1>Give feedback</h1>
       <Button handleClick={() => console.log("good")} text="good" />
       <Button handleClick={() => console.log("neutral")} text="neutral" />
       <Button handleClick={() => console.log("bad")} text="bad" />
+      <h1>Statistics</h1>
+      <Stats statName="good" statNum="0" />
+      <Stats statName="neutral" statNum="0" />
+      <Stats statName="bad" statNum="0" />
     </div>
   );
 };
